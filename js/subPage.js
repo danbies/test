0995cbb0ada2829e508810d8c384084cdf3b242a
danbies <165626471+danbies@users.noreply.github.com>
2024-05-01 14:05:07 +0900

    제이쿼리 2차수정

--- js/subPage.js
+++ js/subPage.js
@@ -16,4 +16,18 @@ $(function(){
    $('.tfrm').click(function(){
         $('.tfrm_op').toggle();
    });
+   //포지션 fixed를 적용해야 가능
+   //하지만 나는 컨텐츠 영역안에서만 움직이고 싶단 말이지
+//    $('#con2').scroll(function(){
+//         if($(this).scrollTop()>100){
+//             $('#toTop').fadeIn();
+//         }else{
+//             $('#toTop').fadeOut();
+//         }
+//    });
+   $('#toTop').click(function(){
+        $('body,html').animate({
+            scrollTop:0
+        },600)
+    });
 });
