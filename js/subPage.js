8bf2a724daad40deb9b81f9aa3108735b007d256
danbies <165626471+danbies@users.noreply.github.com>
2024-05-01 18:27:01 +0900

    자바스크립트 수정3

--- js/subPage.js
+++ js/subPage.js
@@ -1,18 +1,15 @@
 $(function(){
-    let heightToggle=false;
-    $('#filters').click(function(){
-        heightToggle=!heightToggle;
-        if(heightToggle){
-            $('.filter_nav>form').css('height','84px').css('transition','0.5s');
-        }else{
-            $('.filter_nav>form').css('height','0px');
-        }
-   });
-
+    $('.filter_nav>form').hide();
+    $('#filters').on('click',function(){
+        $('.filter_nav>form').slideToggle();
+    });
+    $()
    $('.select_link').click(function(){
         $('.select_link_op').toggle();
    });
-   //아래쪽은 나중에 손대는 걸로
+   $('#chooseColor').click(function(){
+        $('.color_palette').toggle();
+   });
    $('.tfrm').click(function(){
         $('.tfrm_op').toggle();
    });
@@ -28,6 +25,6 @@ $(function(){
    $('#toTop').click(function(){
         $('body,html').animate({
             scrollTop:0
-        },600)
+        },600);
     });
 });
